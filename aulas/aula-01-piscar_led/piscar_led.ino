aafbc33a8a2cb906ed7bd6ffd8779d07aad1a8bd
Aron da Rocha Battistella <aroba14bis@yahoo.com.br>
2026-03-18 12:03:29 -0300

    refactor: improve blink lesson structure and comments

--- aulas/aula-01-piscar_led/piscar_led.ino
+++ aulas/aula-01-piscar_led/piscar_led.ino
@@ -1,19 +1,44 @@
-// ------------------------------------
+// ----------------------------------------------
 // Parque da Ciência Newton Freire Maia
 // Curso de Formação de Professores
 // Atividades Experimentais de Física
 // Utilizando a Plataforma Arduino
-// ------------------------------------
+// ----------------------------------------------
+// Projeto: Piscar LED
+// Descrição:
+// Acende e apaga um LED conectado ao pino digital 2
+// em intervalos de 1 segundo.
+// ----------------------------------------------
 
-uint8_t pinLed = 2; // pino do LED
+static constexpr uint8_t PINO_LED = 2;
+static constexpr unsigned long TEMPO_ACESO_MS = 1000;
+static constexpr unsigned long TEMPO_APAGADO_MS = 1000;
 
-void setup() {
-  pinMode(pinLed, OUTPUT);
+/*
+    Função de configuração inicial do Arduino.
+
+    Esta função é executada apenas uma vez, logo após
+    a placa ser energizada ou reiniciada.
+*/
+void setup()
+{
+    // Define o pino do LED como saída digital.
+    pinMode(PINO_LED, OUTPUT);
 }
 
-void loop() {
-  digitalWrite(pinLed, HIGH);
-  delay(1000);
-  digitalWrite(pinLed, LOW);
-  delay(1000);
+/*
+    Função principal do Arduino.
+
+    Esta função é executada continuamente em loop.
+    Nela, o LED é ligado e desligado em intervalos fixos.
+*/
+void loop()
+{
+    // Liga o LED.
+    digitalWrite(PINO_LED, HIGH);
+    delay(TEMPO_ACESO_MS);
+
+    // Desliga o LED.
+    digitalWrite(PINO_LED, LOW);
+    delay(TEMPO_APAGADO_MS);
 }
